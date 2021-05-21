21fe3ba54c42ce43859b843e86e0091acc7a9870
dijiflex <dijiflex@gmail.com>
2021-05-21 15:29:45 +0300

    Added firebase config

---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chat.css';
+
+function Chat() {
+    return (
+        <div>
+            <h1>I am the Chat Feature</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_Fzec0_-EmBfuKjNJdR3Kn2CKxu8EWMA",
+    authDomain: "whatsapp-mern-e0023.firebaseapp.com",
+    projectId: "whatsapp-mern-e0023",
+    storageBucket: "whatsapp-mern-e0023.appspot.com",
+    messagingSenderId: "758928715723",
+    appId: "1:758928715723:web:e47e54b678165d6d88d04e"
+  };
