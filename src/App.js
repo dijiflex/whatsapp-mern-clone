e03b751a841619e156501fba7c2483568696cfba
dijiflex <dijiflex@gmail.com>
2021-06-04 11:22:15 +0300

    Installed axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from "pusher-js";
 
 function App() {
+    useEffect(() => {
+        
+    }, [])
+    useEffect(() => {
+        const pusher = new Pusher('9437e4eacdd7efa52e4e', {
+            cluster: 'mt1'
+          });
+      
+          var channel = pusher.subscribe('messages');
+          channel.bind('inserted', function(data) {
+            alert(JSON.stringify(data));
+          });
+        return () => {
+           
+        }
+    }, [])
   return (
     <div className="app">
       <div className='app__body'>
